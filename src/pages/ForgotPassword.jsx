386f8d7532a9ec742c977064ddb3814604e26d05
SumayaShemo <sumayaahmedbd@gmail.com>
2023-01-06 19:25:13 +0600

    Forget Password Active done

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -2,12 +2,26 @@ import { useState } from "react";
 import LognIn from "../Asset/Img/login.svg";
 import { Link } from "react-router-dom";
 import OAuth from "../component/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
  const ForgotPassword=()=> {
   const [email,setEmail]=useState("");
   const changeHandler=(e)=>{
     e.preventDefault();
     setEmail(e.target.value)
+  }
+  const onSubmitHandler=async(e)=>{
+   e.preventDefault();
+   try {
+    const auth =getAuth();
+     await sendPasswordResetEmail(auth,email);
+     toast.success("Email was Send");
+    
+   } catch (error) {
+    toast.error("Could Not Send Reset Password")
+   }
+
   }
 
   return (
@@ -18,7 +32,7 @@ import OAuth from "../component/OAuth";
           <img src={LognIn} alt='key' className=" w-full rounded-2xl"/>
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form >
+          <form onSubmit={onSubmitHandler}>
             <input className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition ease-in-out mb-6" type="email" id='email' value={email} onChange={changeHandler}placeholder="Email" autoComplete="off" />
           
            

2780d6e4b2f44b2becb8baa205d8f35f60975e3d
SumayaShemo <sumayaahmedbd@gmail.com>
2023-01-09 21:54:48 +0600

    Header Menu Change Dynamically

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,14 +1,30 @@
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom'
 import Logo from '../Asset/Img/Logo.jpg'
 
 const Header=()=>{
     const location=useLocation();
     const navigate=useNavigate();
+    const [pageStatus,setPageStatus]=useState("Sign In");
+    const auth=getAuth();
+    useEffect(()=>{
+     onAuthStateChanged(auth,(user)=>{
+       if(user){
+        setPageStatus("Profile")
+       }
+       else{
+        setPageStatus("Sign In")
+       }
+     })
+
+    },[auth]);
     function currentLocation (route){
         if(route === location.pathname){
               return true;
         }
     }
+
   return (
     <div className=' bg-white border-b shadow-sm sticky top-0 z-50'>
        <header className=' flex justify-between items-center px-3 max-w-6xl mx-auto'>
@@ -21,8 +37,8 @@ const Header=()=>{
                 && " text-black border-b-blue-400 "}`} onClick={()=>navigate("/")}>Home</li>
                     <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-4 border-b-transparent ${currentLocation("/offers")
                 && " text-black border-b-blue-400 "}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-4 border-b-transparent ${currentLocation("/sign-in")
-                && " text-black border-b-blue-400 "}`} onClick={()=>navigate("/sign-in")}>Sign In</li>
+                    <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-4 border-b-transparent ${(currentLocation("/sign-in") || currentLocation("/profile"))
+                && "text-black border-b-blue-400 "}`} onClick={()=>navigate("/profile")}>{pageStatus}</li>
             
                  </ul>
             </div>

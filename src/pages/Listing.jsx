e99fb2f3d3bb9a9288fee34f03a00929854c30fa
SumayaShemo <sumayaahmedbd@gmail.com>
2023-02-03 09:19:25 +0600

    add listing detail

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,7 +7,9 @@ import { useState } from 'react';
 import { Spinner } from '../component/Spinner';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Navigation, Pagination,EffectFade,Autoplay} from 'swiper';
-import {FaShare} from "react-icons/fa";
+import {FaShare,FaMapMarkerAlt,FaBed,FaBath,FaParking} from "react-icons/fa";
+import {GiLift}from "react-icons/gi";
+
 import 'swiper/css/bundle';
  const  Listing= () => {
   const params=useParams();
@@ -35,7 +37,7 @@ import 'swiper/css/bundle';
   }
   return (
     <main>
-    <Swiper slidesPerView={1} navigation pagination={{type:"progressbar"}} effect="fade" modules={[EffectFade]} autoplay={{delay:600}}>
+    <Swiper slidesPerView={1} navigation pagination={{type:"progressbar"}} effect="fade" modules={[EffectFade]} autoplay={{delay:3000}}>
        {listing.imgUrls.map((url,index)=>(
         <SwiperSlide key={index}>
             <div
@@ -58,6 +60,38 @@ import 'swiper/css/bundle';
     {linkCopy &&(
       <p className=' fixed top-[23%] right-[5%] z-10 font-semibold border-2 border-gray-200 rounded-md bg-white p-2 text-green-400'>Link Copied</p>
     )}
+    <div className=' flex flex-col md:flex-row m-4 max-w-6xl lg:mx-auto p-4 rounded-lg border-3 bg-white shadow-lg lg:space-x-5'>
+      <div className='w-full h-[200px] lg-[400px]'>
+      <p className=' text-2xl font-bold mb-3 text-blue-900'>
+          {listing.name} - BDT {listing.offer?listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","):listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {listing.type==="rent"?" / month":""}
+      </p>
+      <p className=' flex items-center mb-3 mt-6 font-semibold'>
+      <FaMapMarkerAlt className=' mr-1 text-green-600'/>
+      {listing.address}
+      </p>
+       <div className=' flex justify-start items-center space-x-4 w-[75%]'>
+        <p className=' bg-red-800 w-full max-w-[200px] font-semibold rounded-md shadow-md p-1 text-white text-center '>{listing.type==="rent"?"Rent":"Sale"}</p>
+        {
+          listing.offer && (
+          <p className=' w-full max-w-[200px] font-semibold text-white text-center bg-green-800 p-1 rounded-md shadow-md'>BDT {(+listing.regularPrice)- (+listing.discountedPrice)} discount </p> 
+          
+          )
+        }
+  
+       </div>
+       <p className='mt-3 mb-3 whitespace-nowrap'><span className=' font-semibold'>Description - </span>{listing.description}</p>
+       <ul className=' flex items-center space-x-2 sm:space-x-10 font-semibold text-sm'>
+        <li className=' flex items-center'><FaBed className=' text-lg mr-1' /> {+listing.bedrooms>1? `${listing.bedrooms} Beds`:"1 Bed"}</li>
+        <li className=' flex items-center'><FaBath className=' text-lg mr-1' /> {+listing.bathrooms>1? `${listing.bathrooms} Baths`:"1 Bath"}</li>
+        <li className=' flex items-center'><FaParking className=' text-lg mr-1' /> {listing.parking?"Parking Spot":"No Parking"}</li>
+        <li className=' flex items-center'><GiLift className=' text-lg mr-1' /> {listing.lift?"Lift":"No Lift"}</li>
+        
+       </ul>
+      </div>
+      <div className=' bg-blue-400 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden'>
+      
+      </div>
+    </div>
    </main>
   )
 }
